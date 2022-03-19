ae2f07fbf2d43f2bf5c27c669689bfc454ea0697
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-03-19 23:42:28 +0200

    A

--- src/App.js
+++ src/App.js
@@ -8,6 +8,35 @@ import {useSelector} from 'react-redux';
 
 
 
+// ACTIONS
+const increment=()=>{
+  return{
+    type:'INCREMENT'
+  }
+}
+const decrement=()=>{
+  return{
+    type:'DECREMENT'
+  }
+}    
+//Reducer
+const counter = (state=0,action) =>{
+  switch(action.type)
+  {
+    case "INCREMENT":
+      return state+1;
+      break;
+      case "DECREMENT":
+        return state-1;
+        break;
+    }
+}
+
+const store = createStore(counter);
+
+store.subscribe(()=>{console.log(store.getState())});
+
+
 
 function App() {
 
@@ -18,8 +47,13 @@ function App() {
 
       <div onClick={()=>{
          
-        //store.dispatch(increment());
-        console.log(counter);
+        store.dispatch(increment());
+        console.log(store);
+        if(store.getState()%4===0)
+        {
+          store.dispatch(decrement());
+          store.dispatch(decrement());
+        }
         
       }}>
         Start{counter}
